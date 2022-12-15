004aae0fb5d40c31a8df97d6ed187737b135f2ff
Biplob556618 <117024660+Biplob556618@users.noreply.github.com>
2022-12-15 14:30:31 +0600

    add

--- data_class.php
+++ data_class.php
@@ -3,6 +3,23 @@ session_start();
 include("db.php");
 class data extends db
 {
+    private $bookpic;
+    private $bookname;
+    private $bookdetail;
+    private $bookaudor;
+    private $bookpub;
+    private $branch;
+    private $bookprice;
+    private $bookquantity;
+    private $type;
+
+    private $book;
+    private $userselect;
+    private $days;
+    private $getdate;
+    private $returnDate;
+
+
     function __construct(){
 
 
@@ -25,5 +42,25 @@ class data extends db
 
         }
     
+    }
+
+
+    function addnewuser($name, $pasword, $email, $type)
+    {
+$this->name=$name;
+        $this->pasword=$pasword;
+        $this->email=$email;
+        $this->type=$type;
+
+
+         $q="INSERT INTO userdata(id, name, email, pass,type)VALUES('','$name','$email','$pasword','$type')";
+
+        if($this->connection->exec($q)) {
+            header("Location:admin_service_dashboard.php?msg=New Add done");
+        }
+
+        else {
+            header("Location:admin_service_dashboard.php?msg=Register Fail");
+        }
     }
 }
