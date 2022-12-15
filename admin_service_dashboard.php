9d49cb2b75a18452c09f48ae606980c5d0c6fb71
Biplob556618 <117024660+Biplob556618@users.noreply.github.com>
2022-12-15 15:21:27 +0600

    add

--- admin_service_dashboard.php
+++ admin_service_dashboard.php
@@ -193,42 +193,8 @@ elseif($msg=="fail"){
             
                 </div>
 
-            <div class="rightinnerdiv">   
-            <div id="bookrequestapprove" class="innerright portion" style="display:none">
-            <Button class="greenbtn" >BOOK REQUEST APPROVE</Button>
-
-            <?php
-            $u=new data;
-            $u->setconnection();
-           // $u->requestbookdata();
-           // $recordset=$u->requestbookdata();
-
-            $table="<table style='font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;'><tr><th style='
-            padding: 8px;'>Person Name</th><th>person type</th><th>Book name</th><th>Days </th><th>Approve</th></tr>";
-            foreach($recordset as $row){
-                $table.="<tr>";
-               "<td>$row[0]</td>";
-              "<td>$row[1]</td>";
-              "<td>$row[2]</td>";
-
-                $table.="<td>$row[3]</td>";
-                $table.="<td>$row[4]</td>";
-                $table.="<td>$row[5]</td>";
-                $table.="<td>$row[6]</td>";
-               // $table.="<td><a href='approvebookrequest.php?reqid=$row[0]&book=$row[5]&userselect=$row[3]&days=$row[6]'><button type='button' class='btn btn-primary'>Approved BOOK</button></a></td>";
-                 $table.="<td><a href='approvebookrequest.php?reqid=$row[0]&book=$row[5]&userselect=$row[3]&days=$row[6]'><button type='button' class='btn btn-primary'>Approved</button></a></td>";
-                // $table.="<td><a href='deletebook_dashboard.php?deletebookid=$row[0]'>Delete</a></td>";
-                $table.="</tr>";
-                // $table.=$row[0];
-            }
-            $table.="</table>";
-
-            echo $table;
-            ?>
-
-            </div>
-            </div>
-
+            
+         <!--add book part................................-->
             <div class="rightinnerdiv">   
             <div id="addbook" class="innerright portion" style="<?php  if(!empty($_REQUEST['viewid'])){ echo "display:none";} else {echo ""; }?>">
             <Button class="greenbtn" >ADD NEW BOOK</Button>
@@ -239,7 +205,7 @@ elseif($msg=="fail"){
             <label>Detail:</label><input  type="text" name="bookdetail"/></br>
             <label>Autor:</label><input type="text" name="bookaudor"/></br>
             <label>Publication</label><input type="text" name="bookpub"/></br>
-            <div><label>Branch:</label><input type="radio" name="branch" value="other"/>Other<input type="radio" name="branch" value="BSIT"/>BSIT<div style="margin-left:80px"><input type="radio" name="branch" value="BSCS"/>BSCS<input type="radio" name="branch" value="BSSE"/>BSSE</div>
+            <div><label>Branch:</label><input type="radio" name="branch" value="EEE"/>EEE<input type="radio" name="branch" value="CSE"/>CSE<div style="margin-left:80px"><input type="radio" name="branch" value="CS"/>CS<input type="radio" name="branch" value="ENG"/>ENG</div>
             </div>   
             <label>Price:</label><input  type="number" name="bookprice"/></br>
             <label>Quantity:</label><input type="number" name="bookquantity"/></br>
@@ -254,7 +220,7 @@ elseif($msg=="fail"){
             </div>
             </div>
 
-
+<!-- add student part.........................-->
             <div class="rightinnerdiv">   
             <div id="addstudent" class="innerright portion" style="display:none">
             <Button class="greenbtn" >ADD Student</Button>
@@ -267,7 +233,7 @@ elseif($msg=="fail"){
             <label for="typw">Choose type:</label>
             <select name="type" >
                 <option value="student">student</option>
-                <option value="teacher">teacher</option>
+                
             </select>
 
             <input type="submit" value="SUBMIT"/>
@@ -275,189 +241,7 @@ elseif($msg=="fail"){
             </div>
             </div>
 
-            <div class="rightinnerdiv">   
-            <div id="studentrecord" class="innerright portion" style="display:none">
-            <Button class="greenbtn" >Student RECORD</Button>
-
-            <?php
-            $u=new data;
-            $u->setconnection();
-           // $u->userdata();
-           // $recordset=$u->userdata();
-
-            $table="<table style='font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;'><tr><th style=' 
-            padding: 8px;'> Name</th><th>Email</th><th>Type</th></tr>";
-            foreach($recordset as $row){
-                $table.="<tr>";
-               "<td>$row[0]</td>";
-                $table.="<td>$row[1]</td>";
-                $table.="<td>$row[2]</td>";
-                $table.="<td>$row[4]</td>";
-                // $table.="<td><a href='deleteuser.php?useriddelete=$row[0]'>Delete</a></td>";
-                $table.="</tr>";
-                // $table.=$row[0];
-            }
-            $table.="</table>";
-
-            echo $table;
-            ?>
-
-            </div>
-            </div>
-
-            <div class="rightinnerdiv">   
-            <div id="issuebookreport" class="innerright portion" style="display:none">
-            <Button class="greenbtn" >Issue Book Record</Button>
-
-            <?php
-            $u=new data;
-            $u->setconnection();
-           // $u->issuereport();
-            //$recordset=$u->issuereport();
-
-            $table="<table style='font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;'><tr><th style='  
-            padding: 8px;'>Issue Name</th><th>Book Name</th><th>Issue Date</th><th>Return Date</th><th>Fine</th></th><th>Issue Type</th></tr>";
-
-            foreach($recordset as $row){
-                $table.="<tr>";
-               "<td>$row[0]</td>";
-                $table.="<td>$row[2]</td>";
-                $table.="<td>$row[3]</td>";
-                $table.="<td>$row[6]</td>";
-                $table.="<td>$row[7]</td>";
-                $table.="<td>$row[8]</td>";
-                $table.="<td>$row[4]</td>";
-                // $table.="<td><a href='otheruser_dashboard.php?returnid=$row[0]&userlogid=$userloginid'>Return</a></td>";
-                $table.="</tr>";
-                // $table.=$row[0];
-            }
-            $table.="</table>";
-
-            echo $table;
-            ?>
-
-            </div>
-            </div>
-
-<!--             
-
-issue book -->
-            <div class="rightinnerdiv">   
-            <div id="issuebook" class="innerright portion" style="display:none">
-            <Button class="greenbtn" >ISSUE BOOK</Button>
-            <form action="issuebook_server.php" method="post" enctype="multipart/form-data">
-            <label for="book">Choose Book:</label>
-           
-            <select name="book" >
-            <?php
-            $u=new data;
-            $u->setconnection();
-            //$u->getbookissue();
-           // $recordset=$u->getbookissue();
-            foreach($recordset as $row){
-
-                echo "<option value='". $row[2] ."'>" .$row[2] ."</option>";
-        
-            }            
-            ?>
-            </select>
-<br>
-            <label for="Select Student">Select Student:</label>
-            <select name="userselect" >
-            <?php
-            $u=new data;
-            $u->setconnection();
-           // $u->userdata();
-           // $recordset=$u->userdata();
-            foreach($recordset as $row){
-               $id= $row[0];
-                echo "<option value='". $row[1] ."'>" .$row[1] ."</option>";
-            }            
-            ?>
-            </select>
-<br>
-           <label>Days</label> <input type="number" name="days"/>
-
-            <input type="submit" value="SUBMIT"/>
-            </form>
-            </div>
-            </div>
-
-            <div class="rightinnerdiv">   
-            <div id="bookdetail" class="innerright portion" style="<?php  if(!empty($_REQUEST['viewid'])){ $viewid=$_REQUEST['viewid'];} else {echo "display:none"; }?>">
-            
-            <Button class="greenbtn" >BOOK DETAIL</Button>
-</br>
-<?php
-            $u=new data;
-            $u->setconnection();
-           // $u->getbookdetail($viewid);
-           // $recordset=$u->getbookdetail($viewid);
-            foreach($recordset as $row){
-
-                $bookid= $row[0];
-               $bookimg= $row[1];
-               $bookname= $row[2];
-               $bookdetail= $row[3];
-               $bookauthour= $row[4];
-               $bookpub= $row[5];
-               $branch= $row[6];
-               $bookprice= $row[7];
-               $bookquantity= $row[8];
-               $bookava= $row[9];
-               $bookrent= $row[10];
-
-            }            
-?>
-
-            <img width='150px' height='150px' style='border:1px solid #333333; float:left;margin-left:20px' src="uploads/<?php echo $bookimg?> "/>
-            </br>
-            <p style="color:black"><u>Book Name:</u> &nbsp&nbsp<?php echo $bookname ?></p>
-            <p style="color:black"><u>Book Detail:</u> &nbsp&nbsp<?php echo $bookdetail ?></p>
-            <p style="color:black"><u>Book Authour:</u> &nbsp&nbsp<?php echo $bookauthour ?></p>
-            <p style="color:black"><u>Book Publisher:</u> &nbsp&nbsp<?php echo $bookpub ?></p>
-            <p style="color:black"><u>Book Branch:</u> &nbsp&nbsp<?php echo $branch ?></p>
-            <p style="color:black"><u>Book Price:</u> &nbsp&nbsp<?php echo $bookprice ?></p>
-            <p style="color:black"><u>Book Available:</u> &nbsp&nbsp<?php echo $bookava ?></p>
-            <p style="color:black"><u>Book Rent:</u> &nbsp&nbsp<?php echo $bookrent ?></p>
-
-
-            </div>
-            </div>
-
-
-
-            <div class="rightinnerdiv">   
-            <div id="bookreport" class="innerright portion" style="display:none">
-            <Button class="greenbtn" >BOOK RECORD</Button>
-            <?php
-            $u=new data;
-            $u->setconnection();
-           // $u->getbook();
-           // $recordset=$u->getbook();
-
-            $table="<table style='font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;'><tr><th style=' 
-            padding: 8px;'>Book Name</th><th>Price</th><th>Qnt</th><th>Available</th><th>Rent</th></th><th>View</th></tr>";
-            foreach($recordset as $row){
-                $table.="<tr>";
-               "<td>$row[0]</td>";
-                $table.="<td>$row[2]</td>";
-                $table.="<td>$row[7]</td>";
-                $table.="<td>$row[8]</td>";
-                $table.="<td>$row[9]</td>";
-                $table.="<td>$row[10]</td>";
-                $table.="<td><a href='admin_service_dashboard.php?viewid=$row[0]'><button type='button' class='btn btn-primary'>View BOOK</button></a></td>";
-                // $table.="<td><a href='deletebook_dashboard.php?deletebookid=$row[0]'>Delete</a></td>";
-                $table.="</tr>";
-                // $table.=$row[0];
-            }
-            $table.="</table>";
-
-            echo $table;
-            ?>
-
-            </div>
-            </div>
+ 
 
 
 
--- data_class.php
+++ data_class.php
@@ -63,4 +63,28 @@ $this->name=$name;
             header("Location:admin_service_dashboard.php?msg=Register Fail");
         }
     }
+
+    
+        function addbook($bookpic, $bookname, $bookdetail, $bookaudor, $bookpub, $branch, $bookprice, $bookquantity) {
+            $this->$bookpic=$bookpic;
+            $this->bookname=$bookname;
+            $this->bookdetail=$bookdetail;
+            $this->bookaudor=$bookaudor;
+            $this->bookpub=$bookpub;
+            $this->branch=$branch;
+            $this->bookprice=$bookprice;
+            $this->bookquantity=$bookquantity;
+    
+           $q="INSERT INTO book (id,bookpic,bookname, bookdetail, bookaudor, bookpub, branch, bookprice,bookquantity,bookava,bookrent)VALUES('','$bookpic', '$bookname', '$bookdetail', '$bookaudor', '$bookpub', '$branch', '$bookprice', '$bookquantity','$bookquantity',0)";
+    
+            if($this->connection->exec($q)) {
+                header("Location:admin_service_dashboard.php?msg=done");
+            }
+    
+            else {
+                header("Location:admin_service_dashboard.php?msg=fail");
+            }
+    
+        }
+    
 }
